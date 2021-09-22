430918ff0828b32ebab1f1ca08ca1c7c5c85fcb2
sutefu23 <sutefu23@gmail.com>
2021-09-22 09:46:31 +0900

    合計数をデータ数ではなく受注数に変更

--- backend/resources/ts/pages/Orders/Orders.tsx
+++ backend/resources/ts/pages/Orders/Orders.tsx
@@ -8,7 +8,6 @@ import Notification from "../../common/components/Notification";
 import useUpload from "../../hooks/order/useUploadCSV";
 import useGetOrdersQuery from "../../hooks/order/useGetOrders";
 import useGetGroupByItem from "../../hooks/order/useGetGroupByItem";
-
 const Tabs = {
     ITEM_LIST: 'ITEM_LIST',
     ITEM_GROUP: 'ITEM_GROUP',
@@ -73,7 +72,18 @@ const Orders: React.FC = () => {
         () => {
             return orders?.reduce<number>((acc: number, val) => {
                 return acc + val.inclusive_sum
-            },0)
+            },0) ?? 0
+        },
+        [orders]
+    )
+    const orderCount = useCallback(
+        () => {
+            return orders?.reduce<Array<string>>((keys: Array<string>, order) => {
+                if (!keys.find((k) => k === order.reception_number)){
+                    keys.push(order.reception_number)
+                }
+                return keys
+            },[]).length ?? 0
         },
         [orders]
     )
@@ -103,8 +113,8 @@ const Orders: React.FC = () => {
                 orders &&
                     <React.Fragment>
                         <TopCard title="受注日" text={orders[0]?.reception_date.toString()} icon="calendar-alt" class="success" />
-                        <TopCard title="受注数" text={orders.length.toString()} icon="list-ol" class="danger" />
-                        <TopCard title="合計額" text={orderSum()?.toLocaleString()??"0"} icon="calculator" class="success" />
+                        <TopCard title="受注数" text={orderCount().toString()} icon="list-ol" class="danger" />
+                        <TopCard title="合計額" text={orderSum().toLocaleString()} icon="calculator" class="success" />
                     </React.Fragment>
                 }
             </div>
