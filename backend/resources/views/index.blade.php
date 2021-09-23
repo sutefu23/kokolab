92e7f878f0b43a40653072584dddaefc3680d2a3
sutefu23 <sutefu23@gmail.com>
2021-09-23 15:51:55 +0900

    タイトル修正

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+        <title>Kokolab!</title>
         <script src="{{ asset('js/index.js') }}" defer></script>
     </head>
     <body>
