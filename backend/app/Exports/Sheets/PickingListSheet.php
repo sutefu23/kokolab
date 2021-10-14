561fc3ad8e34a6ecbe9db30a5d43dce430511da8
sutefu23 <sutefu23@gmail.com>
2021-10-14 18:13:01 +0900

    ピッキングリスト複数データ出力

--- backend/app/Exports/Sheets/PickingListSheet.php
+++ backend/app/Exports/Sheets/PickingListSheet.php
@@ -39,7 +39,30 @@ class PickingListSheet implements FromCollection, WithMapping, WithHeadings, Wit
      */
     public function collection(): Collection
     {
-        return Orders::all();
+        $orders = Orders::all();
+        $collections = [];
+        foreach ($orders as $order){
+            for ($q = 1; $q <= $order->quantity; $q++) { //個数が1個以上の場合は顧客名などを空欄にしたうえでその分列を追加する
+                $row = [];
+                $is_first_row = ($q===1);
+                $row['item_code'] = $order->item_code;
+                $row['order_id'] = $is_first_row?$order->order_id:"";
+                $row['delivery_target_name_family_name'] = $is_first_row?$order->delivery_target_name_family_name:"";
+                $row['delivery_target_name_name'] = $is_first_row?$order->delivery_target_name_name:"";
+                $row['fixed_term_times'] = $is_first_row?$order->fixed_term_times:"";
+                $row['payment_methods'] = $is_first_row?$order->payment_methods:"";
+                $row['customer_type'] = $is_first_row?$order->customer_type:"";
+                $row['product_name'] = $order->product_name;
+                $row['delivery_specified_date'] = $is_first_row?$order->delivery_specified_date:"";
+                $row['delivery_specified_time'] = $is_first_row?$order->delivery_specified_time:"";
+                $row['communication_field_from_guest'] = $is_first_row?$order->communication_field_from_guest:"";
+                $row['communication_field_in_store'] = $is_first_row?$order->communication_field_in_store:"";
+                $row['feces_type'] = $is_first_row?$order->feces_type:"";
+                $collections[] = (object)$row;
+            }
+        }
+//        \Log::debug($orders);
+        return new Collection($collections);
     }
     /**
      *
