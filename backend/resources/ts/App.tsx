e65b63e5421b9044d5f0b11746ac0a08bef8bc17
sutefu23 <sutefu23@gmail.com>
2024-05-14 14:24:51 +0900

    Laravel10アップグレード

--- backend/resources/ts/App.tsx
+++ backend/resources/ts/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./App.css";
 import "./styles/sb-admin-2.css";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Redirect,
+    Route,
+} from "react-router-dom";
 import Login from "./pages/Account/Login";
 import Admin from "./pages/Admin/Admin";
 import { PrivateRoute } from "./common/components/PrivateRoute";
@@ -11,35 +16,44 @@ import useGetUserQuery from "./hooks/user/useGetUserQuery";
 import Loading from "./common/components/Loading";
 import { RecoilRoot } from "recoil";
 
-
 const App: React.FC = () => {
-  const queryClient = useQueryClient();
-  const { isLoading } = useGetUserQuery({
-    retry: 0,
-    initialData: undefined,
-    onError: () => {
-      queryClient.setQueryData('user', null);
-    },
-  });
+    const queryClient = useQueryClient();
+    const {
+        isLoading,
+        isError,
+        data: user,
+    } = useGetUserQuery({
+        retry: 0,
+        initialData: undefined,
+        onError: () => {
+            queryClient.setQueryData("user", null);
+        },
+    });
 
-  if (isLoading) {
-    return <Loading/>
-  }
+    if (isLoading) {
+        return <Loading />;
+    }
 
-  return (
-    <div className="App" id="wrapper">
-      <Router>
-        <Switch>
-          <PrivateRoute path="/">
-            <RecoilRoot>
-            <Admin />
-            </RecoilRoot>
-          </PrivateRoute>
-          <AccountRoute path="/login"><Login /></AccountRoute>
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App" id="wrapper">
+            <Router>
+                <Switch>
+                    <PrivateRoute path="/" exact>
+                        <RecoilRoot>
+                            <Admin />
+                        </RecoilRoot>
+                    </PrivateRoute>
+                    <AccountRoute path="/login" exact>
+                        <Login />
+                    </AccountRoute>
+                    {/* Catch-all route to handle non-existing paths */}
+                    <Route path="*">
+                        {user ? <Redirect to="/" /> : <Redirect to="/login" />}
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
